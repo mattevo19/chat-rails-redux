a4f1f078bf25ecf389ba002e9278265900a2a945
matt <mattevo10@gmail.com>
2021-10-11 17:00:26 +0200

    1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+puts 'all deleted'
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+names = %w(general berlin react)
+nicknames = %w(Matt annie cecille steffen john)
+
+channels = names.map do |name|
+  Channel.find_or_create_by(name: name)
+end
+puts 'channels created'
+
+users = nicknames.map do |nickname|
+  User.create(email: "#{nickname.downcase}@lewagon.com", password: "testtest")
+end
+puts 'users created'
+
+20.times do
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::Books::Dune.quote
+end
+puts 'messages created'
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
+end
