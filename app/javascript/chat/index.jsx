7fab26e934034676ec4175b021d56672f70b8675
matt <mattevo10@gmail.com>
2021-10-13 17:10:08 +0200

    6

--- app/javascript/chat/index.jsx
+++ app/javascript/chat/index.jsx
@@ -5,15 +5,13 @@ import { Provider } from 'react-redux';
 import { createStore, combineReducers,applyMiddleware } from 'redux';
 import { logger } from 'redux-logger';
 import reduxPromise from 'redux-promise';
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
-import { CreateHistory as history } from 'history';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 // internal modules
 import App from './components/app';
 
 import messagesReducer from './reducers/messages_reducer';
 import channelsReducer from './reducers/channels_reducer';
-import currentUserReducer from './reducers/current_user_reducer'
 
 const channels = JSON.parse(chat_app.dataset.channels).map(c => c.name)
 // State and reducers
@@ -25,17 +23,15 @@ const initialState = {
 const reducers = combineReducers({
   messages: messagesReducer,
   channels: channelsReducer,
-  currentUser: currentUserReducer
 });
 
 const middleWares = applyMiddleware(logger, reduxPromise)
 // render an instance of the component in the DOM
 ReactDOM.render(
   <Provider store={createStore(reducers,initialState,middleWares)}>
-    <Router history={history}>
+    <Router>
       <Switch>
         <Route path='/channels/:channel' component={App} />
-        {/* <Redirect from='/' to='/channels/general'/> */}
       </Switch>
     </Router>
   </Provider>,
